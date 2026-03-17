1357aa531888557512f16ddffb9b656c205b2f5c
Fernán <ferpanz@gmail.com>
2026-03-17 19:34:46 -0300

    NavBar.jsx lista los elementos del menú de navegación, index.html contiene la estructura básica de la página y main.jsx es el punto de entrada de la aplicación React. Las modificaciones realizadas en estos archivos probablemente incluyen cambios en el diseño, la estructura o la funcionalidad de la aplicación. Asegúrate de revisar cada archivo para entender completamente los cambios realizados antes de confirmar el commit.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import { router } from './router/router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -8,6 +9,8 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Helmet>
+        <title>Inicio | ECTECH Software</title>
+        <meta name="description" content="Empresa de desarrollo de software" />
+        <meta property="og:title" content="Inicio | ECTECH Software" />
+        <meta property="og:description" content="Empresa de desarrollo de software" />
+        <link rel="canonical" href="https://ectechso-ftware.com.ar/" />
+      </Helmet>
+      <h1 className='bg-dark text-white p-3'>Home</h1>
+      
     </>
   )
 }
