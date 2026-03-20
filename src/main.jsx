47371841fa4e5a421fcb2da0e1c805101f92225d
Fernán <ferpanz@gmail.com>
2026-03-19 21:16:56 -0300

    cambios menores

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { router } from './router/router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
+import '../src/global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
