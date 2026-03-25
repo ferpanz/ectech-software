b86319931ef1c4977f795cfce5893957fed324ba
Fernán <ferpanz@gmail.com>
2026-03-25 09:45:12 -0300

    animaciones AOS

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 import avatar from './../assets/logos/avatar.jpg'
 
 function SobreNosotros() {
+
+  useEffect(() => {
+AOS.init({ duration: 1000 })
+}, [])
+
   return (
     <>
       
-        <div class="container-fluid bg-dark-subtle">
+      <div class="container-fluid bg-dark-subtle">
           <div class="row p-5">
-            <div class="col-md-6 text-center justify-content-center">
+            <div class="col-md-6 text-center justify-content-center" data-aos="fade-down-right">
               <img class="rounded-circle mb-2 mt-3" height="140" width="140" src={avatar} alt="foto perfil"></img>
               <h2 class="fw-normal">CEO</h2>
               <h3 class="fw-semibold" >Emiliano Gerardo Pérez</h3>
               <p class="mb-0">Programador especializado en backend cuántico</p>
             </div>
-            <div class="col-md-6 text-center justify-content-center">
+            <div class="col-md-6 text-center justify-content-center" data-aos="fade-down-left">
               <img class="rounded-circle mb-2 mt-3" height="140" width="140" src={avatar} alt="foto perfil"></img>
               <h2 class="fw-normal">CEO</h2>
               <h3 class="fw-semibold">Pepito Juárez</h3>
